2aeb45cb86fa0b9c226916675f68a30d317cd8c8
Daniel Valadas <info@danielvaladas.com>
2020-01-22 18:31:28 -0500

    Add project files.

---
+++ Eraware_Dnn_Templates/UserInputForm.cs
@@ -0,0 +1,47 @@
+﻿using System.Drawing;
+using System.Windows.Forms;
+
+namespace Eraware_Dnn_Templates
+{
+    public partial class UserInputForm : Form
+    {
+        private static string rootNamespace;
+        private TextBox namespaceTextBox;
+        private Button createButton;
+
+        public UserInputForm()
+        {
+            this.Size = new Size(155, 265);
+
+            createButton = new Button();
+            createButton.Location = new Point(90, 25);
+            createButton.Size = new Size(50, 25);
+            createButton.Click += CreateButton_Click;
+            this.Controls.Add(createButton);
+
+            namespaceTextBox = new TextBox();
+            namespaceTextBox.Location = new Point(10, 25);
+            namespaceTextBox.Size = new Size(70, 20);
+            this.Controls.Add(namespaceTextBox);
+        }
+
+        private void CreateButton_Click(object sender, System.EventArgs e)
+        {
+            rootNamespace = namespaceTextBox.Text;
+            this.Close();
+        }
+
+        public static string RootNamespace {
+            get
+            {
+                return rootNamespace;
+            }
+            set
+            {
+                rootNamespace = value;
+            }
+        }
+
+        
+    }
+}
---
+++ Eraware_Dnn_Templates/WizardImplementation.cs
@@ -0,0 +1,53 @@
+﻿using EnvDTE;
+using Microsoft.VisualStudio.TemplateWizard;
+using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace Eraware_Dnn_Templates
+{
+    /// <summary>
+    /// This method is called before opening any item that has the OpenInEditor attribute
+    /// </summary>
+    internal class WizardImplementation : IWizard
+    {
+        public void BeforeOpeningFile(ProjectItem projectItem)
+        {
+        }
+
+        public void ProjectFinishedGenerating(Project project)
+        {
+        }
+
+        public void ProjectItemFinishedGenerating(ProjectItem projectItem)
+        {
+        }
+
+        public void RunFinished()
+        {
+        }
+
+        public void RunStarted(object automationObject, Dictionary<string, string> replacementsDictionary, WizardRunKind runKind, object[] customParams)
+        {
+            try
+            {
+                var inputForm = new UserInputForm();
+                inputForm.ShowDialog();
+
+                var rootNamespace = UserInputForm.RootNamespace;
+
+                replacementsDictionary.Add("$rootnamespace$", rootNamespace);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+                throw;
+            }
+        }
+
+        public bool ShouldAddProjectItem(string filePath)
+        {
+            return true;
+        }
+    }
+}
