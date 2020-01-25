c9b3af11c2296e8921220798f233545177aa224f
Daniel Valadas <info@danielvaladas.com>
2020-01-24 23:43:46 -0500

    started build project

---
+++ Eraware_Dnn_Spa_Ef_Di_Stencil/build/Build.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Linq;
+using Nuke.Common;
+using Nuke.Common.Execution;
+using Nuke.Common.Git;
+using Nuke.Common.ProjectModel;
+using Nuke.Common.Tooling;
+using Nuke.Common.Tools.DotNet;
+using Nuke.Common.Tools.GitVersion;
+using Nuke.Common.Utilities.Collections;
+using static Nuke.Common.EnvironmentInfo;
+using static Nuke.Common.IO.FileSystemTasks;
+using static Nuke.Common.IO.PathConstruction;
+using static Nuke.Common.Tools.DotNet.DotNetTasks;
+
+[CheckBuildProjectConfigurations]
+[UnsetVisualStudioEnvironmentVariables]
+class Build : NukeBuild
+{
+    /// Support plugins are available for:
+    ///   - JetBrains ReSharper        https://nuke.build/resharper
+    ///   - JetBrains Rider            https://nuke.build/rider
+    ///   - Microsoft VisualStudio     https://nuke.build/visualstudio
+    ///   - Microsoft VSCode           https://nuke.build/vscode
+
+    public static int Main() => Execute<Build>(x => x.Compile);
+
+    [Parameter("Configuration to build - Default is 'Debug' (local) or 'Release' (server)")]
+    readonly Configuration Configuration = IsLocalBuild ? Configuration.Debug : Configuration.Release;
+
+    [Solution] readonly Solution Solution;
+    private GitRepository GitRepository;
+    private GitVersion GitVersion;
+
+    AbsolutePath ArtifactsDirectory => RootDirectory / "artifacts";
+    Target Clean => _ => _
+        .Before(Restore)
+        .Executes(() =>
+        {
+            EnsureCleanDirectory(ArtifactsDirectory);
+        });
+
+    Target SetupGit => _ => _
+        .Executes(() => {
+            AbsolutePath git = RootDirectory / ".git";
+            if (DirectoryExists(git))
+            {
+                GitRepository = GitRepository.FromLocalDirectory(RootDirectory);
+            }
+            else
+            {
+                Logger.Warn("For proper versioning support, publish this project to a git repository.");
+            }
+        });
+
+    Target Restore => _ => _
+        .Executes(() =>
+        {
+            DotNetRestore(_ => _
+                .SetProjectFile(Solution));
+        });
+
+    Target Compile => _ => _
+        .DependsOn(Restore)
+        .DependsOn(SetupGit)
+        .Executes(() =>
+        {
+            DotNetBuild(_ => _
+                .SetProjectFile(Solution)
+                .SetConfiguration(Configuration)
+                .SetAssemblyVersion(GitVersion?.AssemblySemVer)
+                .SetFileVersion(GitVersion?.AssemblySemFileVer)
+                .SetInformationalVersion(GitVersion?.InformationalVersion)
+                .EnableNoRestore());
+        });
+
+}
