3067d6bfd24d6477e1d963b92a83df6ef60818d8
Daniel Valadas <info@danielvaladas.com>
2021-01-22 01:55:27 -0500

    Implemented darkfx DocFX theme
    To make sure @david-poindexter does not get a screenburn. This default to a dark theme and adds a dark/light switch to select the light or dark theme.

---
+++ Eraware_Dnn_Spa_Ef_Di_Stencil/module/docfx_project/templates/darkfx/styles/toggle-theme.js
@@ -0,0 +1,17 @@
+﻿const sw = document.getElementById("switch-style"), b = document.body;
+if (sw && b) {
+    sw.checked = true;
+    if (window.localStorage && localStorage.getItem("theme") === "light-theme") {
+        sw.checked = false;
+    }
+    b.classList.toggle("dark-theme", sw.checked)
+    b.classList.toggle("light-theme", !sw.checked)
+
+    sw.addEventListener("change", function () {
+        b.classList.toggle("dark-theme", this.checked)
+        b.classList.toggle("light-theme", !this.checked)
+        if (window.localStorage) {
+            this.checked ? localStorage.setItem("theme", "dark-theme") : localStorage.setItem("theme", "light-theme")
+        }
+    })
+} 
