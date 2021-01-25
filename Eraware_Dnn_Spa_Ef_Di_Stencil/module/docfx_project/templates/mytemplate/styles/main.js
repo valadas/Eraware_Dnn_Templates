e06d6800e21a9798c2ad4da06ed949d047811242
Daniel Valadas <info@danielvaladas.com>
2021-01-24 23:48:55 -0500

    Fixes conflict between mermaid and redoc

--- Eraware_Dnn_Spa_Ef_Di_Stencil/module/docfx_project/templates/mytemplate/styles/main.js
+++ Eraware_Dnn_Spa_Ef_Di_Stencil/module/docfx_project/templates/mytemplate/styles/main.js
@@ -1,13 +1,16 @@
-﻿// Copyright (c) Microsoft. All rights reserved. Licensed under the MIT license. See LICENSE file in the project root for full license information.
-
-mermaid.init({ htmlLabels: true, securityLevel: "antiscript" }, $("code.lang-mermaid"));
-
-document.addEventListener("DOMContentLoaded", () => {
+﻿document.addEventListener("DOMContentLoaded", () => {
     const redoc = document.querySelector("redoc");
 
     if (redoc) {
         const mainContent = document.querySelector(".main-content");
         mainContent.innerHTML = "";
         mainContent.appendChild(redoc);
+
+        var redocScript = document.createElement("script");
+        redocScript.type = "text/javascript";
+        redoc.src = "https://cdn.jsdelivr.net/npm/redoc@next/bundles/redoc.standalone.js";
+        document.head.appendChild(redocScript);
     }
+
+    mermaid.init({startOnLoad: false, htmlLabels: true, securityLevel: "antiscript" }, $("code.lang-mermaid"));
 });
