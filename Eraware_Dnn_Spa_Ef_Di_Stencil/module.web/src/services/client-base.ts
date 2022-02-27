8dd2069e50f71b8b0caf8991b547be99ee14bbc3
Daniel Valadas <info@danielvaladas.com>
2022-02-26 22:17:22 -0500

    Update client-base.ts

--- Eraware_Dnn_Spa_Ef_Di_Stencil/module.web/src/services/client-base.ts
+++ Eraware_Dnn_Spa_Ef_Di_Stencil/module.web/src/services/client-base.ts
@@ -1,4 +1,4 @@
-import { DnnServicesFramework } from '@eraware/dnn-elements';
+import { DnnServicesFramework } from '@dnncommunity/dnn-elements';
 export class ClientBase {
 
   private sf: DnnServicesFramework;
